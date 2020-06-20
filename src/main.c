74d0399928f350ec46ab494f590c6c945d8dc1c3
IvicaDuspara <ivicadus@gmail.com>
2020-06-20 16:27:22 +0200

    Changed main.c to take one argument - absolute path to hidraw file in /dev/ which represents controller

--- src/main.c
+++ src/main.c
@@ -3,14 +3,18 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include "parser.h"
-int main () {
+int main (int argc, char **argv) {
+    if(argc != 2) {
+        printf("Program takes exactly one argument for running. Absolute path to hidraw* file which represents controller.\n");
+        exit(-1);
+    }
    unsigned char buffer[32] = {'\0'};
    struct parsed_pair pp;
    char my_buffer[32];
    fgets(my_buffer,32,stdin);
    pp = parse_input(my_buffer,32);
    create_buffer(buffer,32,&pp);
-   int _fd = open("/dev/hidraw1",O_RDWR | O_NONBLOCK);
+   int _fd = open(argv[1],O_RDWR | O_NONBLOCK);
    if(_fd == -1) {
        printf("Could not open device.");
        exit(-1);
